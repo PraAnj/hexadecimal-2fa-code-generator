8776560fb99959ac1d5e4467c8dd734d6124032d
PraAnj <prageethanjula@gmail.com>
2022-01-26 15:09:39 +0530

    code refactored to support PEP8 style

--- src/code-generator.py
+++ src/code-generator.py
@@ -6,81 +6,79 @@ import random
 from pathlib import Path
 from bitarray import bitarray
 
-numbersCount = 16**8
-rootFileName = 'ROOT'
-leafFileName = 'used-randoms.bin'
+NUMBERS_COUNT = 16**8
+ROOT_FOLDER_NAME = 'ROOT'
+LEAF_FILE_NAME = 'used-randoms.bin'
 MAX_LEVELS = 4
-characters = ['0', '1', '2', '3', '4', '5', '6',
+CHARACTERS = ['0', '1', '2', '3', '4', '5', '6',
               '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
-current_dir = os.getcwd()
-root_dir = os.path.join(current_dir, rootFileName)
-magic_numbers_file = '../data/magic-hexa-numbers.txt'
+MAGIC_NUMBERS_FILE = '../data/magic-hexa-numbers.txt'
+ROOT_DIR = os.path.join(os.getcwd(), ROOT_FOLDER_NAME)
 
-
-def markMagicNumbersAsUsed(array):
-    with open(magic_numbers_file) as f:
+def mark_magic_numbers_as_used(array):
+    with open(MAGIC_NUMBERS_FILE) as f:
         for line in f:
             # int identifies hexa automatically and converts to decimal
             decimal = int(line.strip(), 0)
             array[decimal] = True
 
 
-def getRandomSubFolder(current_dir):
+def get_random_sub_folder(current_dir):
     p = Path(current_dir)
     sub_folder_names = [f.name for f in p.iterdir() if f.is_dir()]
-    decimalRand = random.randint(0, len(sub_folder_names)-1)
-    sub_folder = sub_folder_names[decimalRand]
+    random_decimal = random.randint(0, len(sub_folder_names)-1)
+    sub_folder = sub_folder_names[random_decimal]
     is_last_folder = (len(sub_folder_names) == 1)
     return sub_folder, os.path.join(current_dir, sub_folder), is_last_folder
 
 
-def createSubDirs(level, prefix_dir, array):
+def create_sub_dirs(level, prefix_dir, array):
     if (level > MAX_LEVELS):
-        with open(os.path.join(prefix_dir, leafFileName), 'wb') as fh:
+        with open(os.path.join(prefix_dir, LEAF_FILE_NAME), 'wb') as fh:
             array.tofile(fh)
         return
     slice_size = int(len(array)/16)
-    for index, char in enumerate(characters):
+    for index, char in enumerate(CHARACTERS):
         sub_dir = os.path.join(prefix_dir, char)
         os.makedirs(sub_dir)
-        createSubDirs(level+1, sub_dir,
+        create_sub_dirs(level+1, sub_dir,
                       array[index*slice_size:(index+1)*slice_size])
 
 
-def prepareInitialSetup():
-    if not os.path.exists(rootFileName):
-        initialData = bitarray(numbersCount)
-        initialData.setall(0)  # False
-        markMagicNumbersAsUsed(initialData)
-        if not os.path.exists(root_dir):
-            os.makedirs(root_dir)
-        createSubDirs(1, root_dir, initialData)
+def prepare_initial_setup():
+    if not os.path.exists(ROOT_FOLDER_NAME):
+        initial_data = bitarray(NUMBERS_COUNT)
+        initial_data.setall(0)  # False
+        mark_magic_numbers_as_used(initial_data)
+        if not os.path.exists(ROOT_DIR):
+            os.makedirs(ROOT_DIR)
+        create_sub_dirs(1, ROOT_DIR, initial_data)
 
 
-def popRandomHex(level, prefix_dir):
+def pop_random_hexa(level, prefix_dir):
     if (level > MAX_LEVELS):
-        with open(os.path.join(prefix_dir, leafFileName), 'r+b') as f:
+        with open(os.path.join(prefix_dir, LEAF_FILE_NAME), 'r+b') as f:
             mapping = mmap.mmap(f.fileno(), 0)
             # endian='big' by default, can change to little
             array = bitarray(buffer=mapping)
-            indexes = [i for i, bit in enumerate(array) if bit == False]
-            if (len(indexes) > 0):
-                index = random.randint(0, len(indexes)-1)
-                decimalRand = indexes[index]
-                array[decimalRand] = True       # persist used number
-                delete_me = (len(indexes) == 1)  # if last index is choosen
-                return format(decimalRand, 'X'), delete_me
+            indices = [i for i, bit in enumerate(array) if bit is False]
+            if (len(indices) > 0):
+                index = random.randint(0, len(indices)-1)
+                random_decimal = indices[index]
+                array[random_decimal] = True       # persist used number
+                delete_me = (len(indices) == 1)  # if last index is choosen
+                return format(random_decimal, 'X'), delete_me
 
-    sub_folder_name, sub_dir, is_last_folder = getRandomSubFolder(prefix_dir)
-    hexa_rand, delete_me = popRandomHex(level+1, sub_dir)
+    sub_folder_name, sub_dir, is_last_folder = get_random_sub_folder(prefix_dir)
+    hexa_rand, delete_me = pop_random_hexa(level+1, sub_dir)
     if delete_me:
         os.remove(sub_dir)
         delete_me = is_last_folder
     return sub_folder_name + hexa_rand, delete_me
 
 
-prepareInitialSetup()
-hex_code, delete_me = popRandomHex(1, root_dir)
+prepare_initial_setup()
+hex_code, delete_me = pop_random_hexa(1, ROOT_DIR)
 print("0x" + hex_code)
 if delete_me:
-    os.remove(root_dir)  # deleting root finally
+    os.remove(ROOT_DIR)  # deleting root finally
